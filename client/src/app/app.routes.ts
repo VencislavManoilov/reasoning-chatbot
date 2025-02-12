07c5c7cdaab1733e14dcbfc92789584ceaebeea4
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2025-02-12 22:54:19 +0200

    👯‍♂️ Fixed the rendering logic

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register.component';
 import { LoginComponent } from './auth/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent},
     { path: 'auth/register', component: RegisterComponent },
     { path: 'auth/login', component: LoginComponent },
     { path: 'profile', component: ProfileComponent }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
