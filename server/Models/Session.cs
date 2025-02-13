acb52dba44fe156d55fc3ed633ac4a166451490f
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2025-02-13 21:00:22 +0200

    🏞 Changed from config files to env mostly

--- server/Models/Session.cs
+++
@@ -1,7 +0,0 @@
-namespace App.Models {
-    public class session_data {
-        public required string id { get; set; }
-        public required byte[] value { get; set; }
-        public DateTime expires_at { get; set; }
-    }
-}
